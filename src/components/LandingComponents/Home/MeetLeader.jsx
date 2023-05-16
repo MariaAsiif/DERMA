faa430112c9189c982f72a57d9c9cea4784a9fc8
laherasif <laherasif@gmail.com>
2023-05-16 11:17:50 +0500

    set heading about us

--- src/components/LandingComponents/Home/MeetLeader.jsx
+++ src/components/LandingComponents/Home/MeetLeader.jsx
@@ -7,8 +7,8 @@ const MeetLeader = () => {
       <div className='container mx-auto  xl:px-10 px-5 pt-[80px] mb-[80px] relative'>
         <div className='border-4 lg:h-[400px] rounded-2xl  border-[#C9E065] lg:px-0 px-3 lg:py-0 py-3'>
           <div className='max-w-[800px] lg:ml-[40%] md:ml-[45%] lg:pt-0 md:pt-0 pt-[100%]'>
-            <h2 className='text-[#C9E065]  leading-0  text-[60px] font-normal font-Herr text-center'>About <br /> Us</h2>
-            <h1 className='text-[#1F3D64]  -mt-[120px] uppercase leading-0 lg:text-[36px] text-[20px] font-sans font-medium'>Meet Our Leading Specialist</h1>
+            <h2 className='text-[#C9E065]  leading-0  text-[60px] font-normal font-Herr text-center'>About Us</h2>
+            <h1 className='text-[#1F3D64]  -mt-[30px] uppercase leading-0 lg:text-[36px] text-[20px] font-sans font-medium'>Meet Our Leading Specialist</h1>
             <p className='text-[14px] text-[#1C1C1C] leading-[25px] text-justify max-w-[550px] font-sans  font-normal'>Dr Maham Khan is an experienced doctor known for her warmth, approachability, and dedication to dermatology. Having trained at the renowned Imperial College Healthcare she now works at leading hospitals in London. She is an internationally renowned dermatologist, who has worked within the NHS for many years. She has gained further experience in the field by doing Masters in Clinical Dermatology from the prestigious St John's Institute of Dermatology. She has carried out cosmetic Dermatology Procedures at Harley Street, London. She has a special interest in acne, skin cancer and inflammatory skin conditions. She believes in a holistic approach to patient management. </p>
 
             <button className='rounded-full lg:text-[16px] text-[14px] font-bold font-sans mt-3 bg-[#1F3D64] text-[#C9E065] hover:bg-[#C9E065] hover:text-[#1F3D64] uppercase px-8 py-3 '>book an appointment</button>
